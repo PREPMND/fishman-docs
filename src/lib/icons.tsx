4789b1b3ced6a419eb13f3dd8c2fb7a06619fc5b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-29 05:46:02 +0000

    Aug 29, 2026, 11:16 AM

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -22,7 +22,7 @@ export const docIcons: Record<string, LucideIcon> = {
   '/api-reference': Code2,
   '/changelog': History,
   '/contributing': GitPullRequest,
-  '/git-features':GitBranch
+  '/git':GitBranch
 }
 
 export function getDocIcon(href: string): LucideIcon {
